716dc194ecd63aa79f2fee0dc635c45850f529d1
Jorge Redondo Flames <jorge.redondo@vascarsolutions.com>
2020-06-09 19:19:24 -0400

    Added some stuff to CEP for debugging. Not working.

--- cep/src/main/java/org/streamexperiments/cep/process/CEP.java
+++ cep/src/main/java/org/streamexperiments/cep/process/CEP.java
@@ -13,6 +13,19 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
+import org.apache.flink.util.Collector;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.*;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -22,6 +35,7 @@ public class CEP {
 
     public static final String KAFKA_TOPIC_PRODUCER = "producer.topic";
     public static final String KAFKA_TOPIC_CONSUMER = "consumer.topic";
+    private static Logger logger = LogManager.getLogger(CEP.class);
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -29,6 +43,8 @@ public class CEP {
 
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "192.168.99.147:9092");
+        properties.setProperty("zookeeper.connect", "192.168.99.147:2181");
+        properties.setProperty("group.id", "siTestGroup");
 
         TypeInformation<Update> typeInfo = TypeExtractor.getForClass(Update.class);
         TypeInformationSerializationSchema<Update> schema =
@@ -36,23 +52,56 @@ public class CEP {
 
 
         DataStream<Update> incomingStream = env
-                .addSource(new FlinkKafkaConsumer<>(KAFKA_TOPIC_PRODUCER, schema, properties));
+                .addSource(new FlinkKafkaConsumer<Update>(KAFKA_TOPIC_PRODUCER, schema, properties))
+                .setParallelism(1)
+                .flatMap(
+                        new LogFlatMapFunction())
+                .setParallelism(1);
 
+        DataStream<Collection<Update>> windowedStream15secs = incomingStream
+                .keyBy((KeySelector<Update, String>) Update::getSender)
+                .timeWindow(Time.seconds(15), Time.seconds(5))
+                // Tumbling windows for testing
+                //.timeWindow(Time.seconds(15))
+                .apply(new WindowedUpdatesFunction());
+
+
+/*
         UpdateSerializationSchema kafkaSchema = new UpdateSerializationSchema(KAFKA_TOPIC_CONSUMER);
 
         FlinkKafkaProducer<Update> kafkaProducer = new FlinkKafkaProducer<>(KAFKA_TOPIC_CONSUMER,
                 kafkaSchema,
                 properties,
-                FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
+                FlinkKafkaProducer.Semantic.NONE);
 
         kafkaProducer.setWriteTimestampToKafka(true);
 
         DataStreamSink<Update> returnStream = incomingStream.addSink(kafkaProducer);
 
         returnStream.setParallelism(1);
+*/
 
         env.execute("Producer/Flink/Consumer Demonstration");
 
+    }
+
+    public static class WindowedUpdatesFunction implements WindowFunction<Update, Collection<Update>, String, TimeWindow> {
+
+        @Override
+        public void apply(String key, TimeWindow window, Iterable<Update> updates, Collector<Collection<Update>> out) {
+            Set<Update> set = new HashSet<>();
+            
+            for(Update update: updates) 
+                set.add(update);
+        }  
+    }
+
+    public static class LogFlatMapFunction implements FlatMapFunction<Update, Update> {
+        @Override
+        public void flatMap(Update update, Collector<Update> out) {
+            System.out.println("TESTING: " + update.toString());
+            out.collect(update);
+        }
     }
 
     public static class UpdateSerializationSchema implements KafkaSerializationSchema<Update> {
@@ -74,7 +123,7 @@ public class CEP {
             try {
                 blob = mapper.writeValueAsBytes(update);
             } catch (JsonProcessingException e) {
-                System.out.println(e.getMessage());
+                logger.error(e.getMessage());
             }
             return new ProducerRecord<>(topic, blob);
         }
