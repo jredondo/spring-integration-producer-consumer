95a3a23ba08f451e9d00bee69c28b20d025c49a1
Jorge Redondo Flames <jorge.redondo@vascarsolutions.com>
2020-06-10 18:58:41 -0400

    Loading CEP configuration from application.yaml

--- cep/src/main/java/org/streamexperiments/cep/process/CEP.java
+++ cep/src/main/java/org/streamexperiments/cep/process/CEP.java
@@ -8,6 +8,7 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.org.yaml.snakeyaml.Yaml;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
@@ -30,27 +31,29 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.InputStream;
 import java.util.*;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.streamexperiments.cep.config.Configuration;
 import org.streamexperiments.models.Update;
 
 public class CEP {
 
-    public static final String KAFKA_TOPIC_PRODUCER = "producer.topic";
-    public static final String KAFKA_TOPIC_CONSUMER = "consumer.topic";
     private static Logger logger = LogManager.getLogger(CEP.class);
+    private static final String CONFIG = "application.yaml";
 
     public static void main(String[] args) throws Exception {
+        final Configuration config = loadConfiguration();
+        final String KAFKA_TOPIC_PRODUCER = config.getProducerTopic();
+        final String KAFKA_TOPIC_CONSUMER = config.getConsumerTopic();
+        final Properties properties = loadProperties(config);
+
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "192.168.99.147:9092");
-        properties.setProperty("zookeeper.connect", "192.168.99.147:2181");
-        properties.setProperty("group.id", "siTestGroup");
-
         UpdateDeserializationSchema deserializationSchema = new UpdateDeserializationSchema();
+        UpdateSerializationSchema serializationSchema = new UpdateSerializationSchema(KAFKA_TOPIC_CONSUMER);
 
         DataStream<Update> incomingStream = env
                 .addSource(new FlinkKafkaConsumer<Update>(KAFKA_TOPIC_PRODUCER, deserializationSchema, properties))
@@ -67,22 +70,37 @@ public class CEP {
                 .apply(new WindowedUpdatesFunction());
 
 
-
-        UpdateSerializationSchema serializationSchema = new UpdateSerializationSchema(KAFKA_TOPIC_CONSUMER);
-
-        FlinkKafkaProducer<Update> kafkaProducer = new FlinkKafkaProducer<>(KAFKA_TOPIC_CONSUMER,
-                serializationSchema,
-                properties,
-                FlinkKafkaProducer.Semantic.NONE);
-
-        kafkaProducer.setWriteTimestampToKafka(true);
+        FlinkKafkaProducer<Update> kafkaProducer = buildKafkaProducer(KAFKA_TOPIC_CONSUMER, properties, serializationSchema);
 
         DataStreamSink<Update> returnStream = incomingStream.addSink(kafkaProducer);
 
         returnStream.setParallelism(1);
 
         env.execute("Producer/Flink/Consumer Demonstration");
+    }
 
+    private static FlinkKafkaProducer<Update> buildKafkaProducer(String KAFKA_TOPIC_CONSUMER, Properties properties, UpdateSerializationSchema serializationSchema) {
+        FlinkKafkaProducer<Update> kafkaProducer = new FlinkKafkaProducer<>(KAFKA_TOPIC_CONSUMER,
+                serializationSchema,
+                properties,
+                FlinkKafkaProducer.Semantic.NONE);
+        kafkaProducer.setWriteTimestampToKafka(true);
+        return kafkaProducer;
+    }
+
+    private static Properties loadProperties(Configuration config) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", config.getBootstrapServers());
+        properties.setProperty("zookeeper.connect", config.getZookeeperConnect());
+        properties.setProperty("group.id", config.getGroupId());
+        return properties;
+    }
+
+    private static Configuration loadConfiguration() {
+        Yaml yaml = new Yaml();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream in = classLoader.getResourceAsStream(CONFIG);
+        return yaml.loadAs(in, Configuration.class);
     }
 
     public static class WindowedUpdatesFunction implements WindowFunction<Update, Collection<Update>, String, TimeWindow> {
@@ -95,14 +113,14 @@ public class CEP {
                 set.add(update);
             }
 
-            System.out.println("Window processing " + set.size() + " updates.");
+            logger.info("Window processing " + set.size() + " updates.");
         }  
     }
 
     public static class LogFlatMapFunction implements FlatMapFunction<Update, Update> {
         @Override
         public void flatMap(Update update, Collector<Update> out) {
-            System.out.println("TESTING: " + update.toString());
+            logger.info("TESTING flatMap: " + update.toString());
             out.collect(update);
         }
     }
@@ -129,7 +147,6 @@ public class CEP {
     }
 
     public static class UpdateSerializationSchema implements KafkaSerializationSchema<Update> {
-
         private String topic;
         private ObjectMapper mapper = new ObjectMapper();
 
