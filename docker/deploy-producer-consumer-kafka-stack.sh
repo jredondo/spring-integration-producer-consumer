63cbf988d478a62d1c280740b038a7f0128f49e7
Jorge Redondo Flames <jorge.redondo@vascarsolutions.com>
2020-07-12 14:02:42 -0400

    Fixing deploy shell script

--- docker/deploy-producer-consumer-kafka-stack.sh
+++ docker/deploy-producer-consumer-kafka-stack.sh
@@ -1,8 +1,13 @@
 echo "Deploying Zookeeper, Kafka and Producer..."
-COMPOSE_FILE=/hosthome/jredondo/Projects/spring-integration-producer-consumer/docker/docker-producer-kafka-stack.yml
-docker-machine ssh manager1 REGISTRY_IP=192.168.99.147 docker stack deploy -c $COMPOSE_FILE producer_consumer
+
+COMPOSE_FILE=docker/docker-producer-kafka-stack.yml
+eval $(docker-machine env manager1)
+
+REGISTRY_IP=192.168.99.147 docker stack deploy -c $COMPOSE_FILE producer_consumer
 sleep 60
 echo "Deploying Consumer-..."
-COMPOSE_FILE=/hosthome/jredondo/Projects/spring-integration-producer-consumer/docker/docker-only-consumer-stack.yml
-docker-machine ssh manager1 REGISTRY_IP=192.168.99.147 docker stack deploy -c $COMPOSE_FILE producer_consumer
+COMPOSE_FILE=docker/docker-only-consumer-stack.yml
+REGISTRY_IP=192.168.99.147 docker stack deploy -c $COMPOSE_FILE producer_consumer
+
+docker-machine env --unset
 
