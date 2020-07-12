f64ee955f0263af539e2975a54a9ec879a0b6f5e
Jorge Redondo Flames <jorge.redondo@vascarsolutions.com>
2020-07-12 13:03:34 -0400

    Some uncomment

--- docker/start-swarm.sh
+++ docker/start-swarm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function startMachine {
+startMachine () {
     docker-machine stop $1
     docker-machine start $1
     docker-machine ssh $1 sudo cp $CERT /usr/local/share/ca-certificates/
@@ -10,8 +10,8 @@ CERT=/hosthome/jredondo/Projects/data_monitor/docker/certs/domain.crt
 
 startMachine manager1 
 startMachine worker0
-#startMachine worker1
-#startMachine worker2
+startMachine worker1
+startMachine worker2
 
 
 
